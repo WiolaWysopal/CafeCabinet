41ebca439940f1d25a949c637aacbc7b9f8f188e
wwysopal <wioletta.wysopal@gmail.com>
2024-07-30 12:30:39 +0200

    Create user table using docker

---
+++ com.cafecabinet/src/main/java/com/cafecabinet/POJO/User.java
@@ -0,0 +1,44 @@
+package com.cafecabinet.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+
+    // serialVersionUID - used during deserialization process to ensure
+    // that the version of the class during serialization and deserialization is consistent.
+    // This is recommended to avoid class versioning issues.
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id") //Column name in database
+    private Integer id; //Name of the variable in the entity
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="contactNumber")
+    private String contactNumber;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="role")
+    private String role;
+}
--- database/dbinit.sql
+++ database/dbinit.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `CafeDB`;
 USE `CafeDB`;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    contactNumber VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    status VARCHAR(255),
+    role VARCHAR(255)
+);
