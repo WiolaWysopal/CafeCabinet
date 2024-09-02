f5b28d57c157546bd50c50644795cf5112e245ff
Wiola <wioletta.wysopal@gmail.com>
2024-09-02 15:39:10 +0200

    Configure signup api of user and structure

--- com.cafecabinet/src/main/java/com/cafecabinet/serviceImpl/UserServiceImpl.java
+++ com.cafecabinet/src/main/java/com/cafecabinet/serviceImpl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -22,15 +23,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         log.info("Inside signup {}", requestMap);
-        if (validateSignUpMap(requestMap)) {
-
-            User user = userDao.findByEmailId(requestMap.get("email"));
-
+        try {
+            if (validateSignUpMap(requestMap)) {
+                User user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                    return CafeUtils.getResponseEntity("Successfully Registered.", HttpStatus.OK);
+                } else {
+                    return CafeUtils.getResponseEntity("Email already exists.", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstant.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        else {
-            return CafeUtils.getResponseEntity(CafeConstant.INVALID_DATA, HttpStatus.BAD_REQUEST);
-        }
-        return null;
+        return CafeUtils.getResponseEntity(CafeConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private boolean validateSignUpMap(Map<String, String> requestMap) {
@@ -39,7 +47,17 @@ public class UserServiceImpl implements UserService {
         {
             return true;
         }
-        else
-            return false;
+        return false;
+    }
+    private User getUserFromMap(Map<String, String> requestMap) {
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus("false");
+        user.setRole("user");
+
+        return user;
     }
 }
--- database/dbinit.sql
+++ database/dbinit.sql
@@ -4,7 +4,7 @@ USE `CafeDB`;
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    contactNumber VARCHAR(255),
+    contact_number VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
     status VARCHAR(255),
